552b0bd0e4cef0f6a624307191983ed05a1d2157
ashim-sharma-console <ashimsharma49@gmail.com>
2025-07-22 12:19:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FlightSearch from "./pages/FlightSearch";
+import FlightBooking from "./pages/FlightBooking";
+import Confirmation from "./pages/Confirmation";
+import LandingPage from "./pages/LandingPage";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/flight-search" element={<FlightSearch />} />
+          <Route path="/flight-booking" element={<FlightBooking />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
