ba9f2fddd7e90f00a53cfaedef3ba3b5bb4ea33c
ashim-sharma-console <ashimsharma49@gmail.com>
2025-07-22 12:53:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/App.css"; 
-
-const LandingPage = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Welcome to Phoenix Airlines</h1>
-      <img
-        src="https://storage.googleapis.com/acciojob-open-file-collections/landing.png"
-        alt="Landing"
-        style={{ width: "60%", maxWidth: "600px" }}
-      />
-    </div>
-  );
-};
-
-const FlightSearch = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Search Flights</h2>
-      <img
-        src="https://storage.googleapis.com/acciojob-open-file-collections/flight-search.png"
-        alt="Flight Search"
-        style={{ width: "60%", maxWidth: "600px" }}
-      />
-    </div>
-  );
-};
-
-const FlightBooking = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Passenger Details</h2>
-      <img
-        src="https://storage.googleapis.com/acciojob-open-file-collections/flight-booking.png"
-        alt="Flight Booking"
-        style={{ width: "60%", maxWidth: "600px" }}
-      />
-    </div>
-  );
-};
-
-const Confirmation = () => {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Booking Confirmed!</h2>
-      <img
-        src="https://storage.googleapis.com/acciojob-open-file-collections/confirmation.png"
-        alt="Confirmation"
-        style={{ width: "60%", maxWidth: "600px" }}
-      />
-    </div>
-  );
-};
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import FlightSearch from "./pages/FlightSearch";
+import FlightBooking from "./pages/FlightBooking";
+import Confirmation from "./pages/Confirmation";
 
 const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/flight-search" element={<FlightSearch />} />
-          <Route path="/flight-booking" element={<FlightBooking />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/flight-search" element={<FlightSearch />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
     </div>
   );
 };
