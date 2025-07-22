c034e9eb0dd4eae9f5b388ef5309c5f9789ba221
ashim-sharma-console <ashimsharma49@gmail.com>
2025-07-22 12:31:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/App.css"; // তোমার CSS থাকলে এখানে সংযুক্ত থাকবে
+import "./styles/App.css"; 
 
 const LandingPage = () => {
   return (
